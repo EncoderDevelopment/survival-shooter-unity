4777d4289b9e0f18986a4f9aa4ed7dbfd12ee9c1
Roberto Andrade <signa.roberto@gmail.com>
2018-09-24 23:09:06 -0400

    All files
    All files

---
+++ Survival Shooter/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    bool playerInRange;
+    float timer;
+
+    Light enemyLight;
+
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player");
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent<EnemyHealth>();
+        anim = GetComponent <Animator> ();
+
+        //enemyLight = GetComponent<Light>();
+    }
+
+
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            //enemyLight.enabled = true;
+            playerInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit (Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            //enemyLight.enabled = false;
+            playerInRange = false;
+        }
+    }
+
+
+    void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
+        {
+            Attack ();
+        }
+
+        if(playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger ("PlayerDead");
+        }
+    }
+
+
+    void Attack ()
+    {
+        timer = 0f;
+
+        if(playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage (attackDamage);
+        }
+    }
+}
